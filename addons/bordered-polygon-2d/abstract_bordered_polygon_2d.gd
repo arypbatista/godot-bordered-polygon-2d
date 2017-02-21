ad75f3a38164bb56defa2c5325108ed0bdefc537
Ary Pablo Batista <arypbatista@gmail.com>
2017-02-21 00:47:54 -0300

    Extracted API methods and options into an abstract class

---
+++ addons/bordered-polygon-2d/abstract_bordered_polygon_2d.gd
@@ -0,0 +1,91 @@
+extends Polygon2D
+
+#
+# AbstractBorderedPolygon2D
+#
+# Holds the API and exported options.
+#
+
+export(int) var border_size = 50 setget set_border_size
+export(int) var border_overlap = 25 setget set_border_overlap
+
+# You can set a tileset with border textures.
+# Tileset must have 2^n sprites. Sprites textures will be used
+# as borders. You must order the sprites in the tileset clockwise so
+# first tile will be the upper border.
+#
+# How will this work? Few examples:
+# - Tileset with 1 sprite will use same border everywhere
+# - Tileset with 2 sprite will use one sprite for top/left borders
+#   and a different one for right/bottom borders
+# - Tilesets with 4 sprites will use different sprites for
+#   top/left/bottom/right borders but diagonal will use the nearest.
+# - Tileset with 8 sprtes will apply 8 different borders to 8 different
+#   directions.
+# - Tilesets with 16 sprites will be really smooth.
+# - etcetera...
+export (TileSet) var border_textures = null setget set_border_textures
+# Border textures will be rotated clock wise to the left
+export (int) var border_clockwise_shift = 0 setget set_border_clockwise_shift
+
+export (Texture) var border_texture = null setget set_border_texture
+export (CanvasItemMaterial) var border_material = null setget set_border_material
+export (Vector2) var border_texture_scale = Vector2(1,1) setget set_border_texture_scale
+export (Vector2) var border_texture_offset = Vector2(0,0) setget set_border_texture_offset
+export (float) var border_texture_rotation = 0.0 setget set_border_texture_rotation
+export (float, 0.0, 1.0, 0.1) var smooth_level = 0.0 setget set_smooth_level
+export (int, 0, 179) var smooth_max_angle = 90 setget set_smooth_max_angle
+
+
+func set_border_size(value):
+	border_size = value
+	update()
+
+func set_border_overlap(value):
+	border_overlap = value
+	update()
+
+func set_border_texture_offset(value):
+	border_texture_offset = value
+	update()
+
+func set_border_texture_scale(value):
+	border_texture_scale = value
+	update()
+
+func set_border_texture_rotation(value):
+	border_texture_rotation = value
+	update()
+
+func set_border_material(value):
+	border_material = value
+	update()
+
+func set_border_texture(value):
+	border_texture = value
+	update()
+
+func set_border_textures(value):
+	border_textures = value
+	update()
+
+func set_border_clockwise_shift(value):
+	border_clockwise_shift = value
+	update()
+
+func set_smooth_level(value):
+	smooth_level = value
+	update()
+
+func set_smooth_max_angle(value):
+	smooth_max_angle = value
+	update()
+
+func set_polygon(polygon):
+	.set_polygon(polygon)
+	create_inner_polygon()
+	update()
+
+func create_inner_polygon():
+	# Subclass Responsibility
+	pass
--- addons/bordered-polygon-2d/bordered_polygon_2d.gd
+++ addons/bordered-polygon-2d/bordered_polygon_2d.gd
@@ -1,38 +1,8 @@
 tool
+extends 'abstract_bordered_polygon_2d.gd'
 # Authors: Jonathan T. Arnold and Ary Pablo Batista
 
 #!!!!!!!! to make borders work you must turn on repeat on textures
-
-extends Polygon2D
-export(int) var border_size = 50 setget set_border_size
-export(int) var border_overlap = 25 setget set_border_overlap
-
-# You can set a tileset with border textures.
-# Tileset must have 2^n sprites. Sprites textures will be used
-# as borders. You must order the sprites in the tileset clockwise so
-# first tile will be the upper border.
-#
-# How will this work? Few examples:
-# - Tileset with 1 sprite will use same border everywhere
-# - Tileset with 2 sprite will use one sprite for top/left borders
-#   and a different one for right/bottom borders
-# - Tilesets with 4 sprites will use different sprites for
-#   top/left/bottom/right borders but diagonal will use the nearest.
-# - Tileset with 8 sprtes will apply 8 different borders to 8 different
-#   directions.
-# - Tilesets with 16 sprites will be really smooth.
-# - etcetera...
-export (TileSet) var border_textures = null setget set_border_textures
-# Border textures will be rotated clock wise to the left
-export (int) var border_clockwise_shift = 0 setget set_border_clockwise_shift
-
-export (Texture) var border_texture = null setget set_border_texture
-export (CanvasItemMaterial) var border_material = null setget set_border_material
-export (Vector2) var border_texture_scale = Vector2(1,1) setget set_border_texture_scale
-export (Vector2) var border_texture_offset = Vector2(0,0) setget set_border_texture_offset
-export (float) var border_texture_rotation = 0.0 setget set_border_texture_rotation
-export (float, 0.0, 1.0, 0.1) var smooth_level = 0.0 setget set_smooth_level
-export (int, 0, 179) var smooth_max_angle = 90 setget set_smooth_max_angle
 
 const QUAD_TOP_1    = 1
 const QUAD_TOP_2    = 0
@@ -47,7 +17,6 @@ const SMOOTH_MAX_NODES_PER_FACE = 10
 var inner_polygon = null
 
 var clockwise = null
-
 
 onready var _is_ready = true
 
@@ -90,11 +59,6 @@ func is_clockwise():
 		clockwise = is_clockwise_shape(get_polygon())
 	return clockwise
 
-func set_polygon(polygon):
-	.set_polygon(polygon)
-	create_inner_polygon()
-	update()
-
 func create_inner_polygon():
 	var p = Polygon2D.new()
 	p.set_texture(get_texture())
@@ -121,52 +85,8 @@ func set_inner_polygon(polygon):
 	else: # polygon is Polygon2D node
 		set_inner_polygon_node(polygon)
 
-func set_border_size(value):
-	border_size = value
-	update()
-
-func set_border_overlap(value):
-	border_overlap = value
-	update()
-
-func set_border_texture_offset(value):
-	border_texture_offset = value
-	update()
-
-func set_border_texture_scale(value):
-	border_texture_scale = value
-	update()
-
-func set_border_texture_rotation(value):
-	border_texture_rotation = value
-	update()
-
-func set_border_material(value):
-	border_material = value
-	update()
-
-func set_border_texture(value):
-	border_texture = value
-	update()
-
-func set_border_textures(value):
-	border_textures = value
-	update()
-
-func set_border_clockwise_shift(value):
-	border_clockwise_shift = value
-	update()
-
-func set_smooth_level(value):
-	smooth_level = value
-	update()
-
 func get_smooth_max_nodes():
 	return get_polygon().size() * SMOOTH_MAX_NODES_PER_FACE * smooth_level
-
-func set_smooth_max_angle(value):
-	smooth_max_angle = value
-	update()
 
 func get_smooth_max_angle():
 	#var smooth_range = smooth_max_angle - SMOOTH_MIN_ANGLE
